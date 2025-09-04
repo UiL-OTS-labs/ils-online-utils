ea81a40b554a5596e87c748963ccc7a8fe7b8860
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-09-04 12:02:51 +0200

    Indent with 4 spaces

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src/"),
+        },
     },
-  },
-  // ...
+    // ...
 });
