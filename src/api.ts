0a10f28c8a919d82adc81dd568d30a8190b549c7
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-09-08 14:45:58 +0200

    Port from java- to typescript

--- src/api.ts
+++ src/api.ts
@@ -1,40 +1,47 @@
+
+/**
+ * type for http request methods
+ */
+type Method = "CONNECT" | "DELETE" | "GET" | "HEAD" | "OPTIONS" | "PATCH" | "POST" | "PUT" | "TRACE" ;
+
 /**
  * Class for handling all requests to the server
  */
 class API {
+    host: URL;
     /**
      * Initializes the api connection
-     * @param {string} host - base URL for all requests (should include https://)
+     * @param host - base URL for all requests (should include https://)
      */
-    constructor(host) {
-        this.host = host;
+    constructor(host: URL | string) {
+        this.host = new URL(host);
     }
 
     /**
      * Performs a request
-     * @param {string} url - URL for the request, relative to the base URL used when initializing the API class
-     * @param {string} method - HTTP method (e.g. GET/POS)
-     * @param {string} data - Data to be sent to the server, in plain text
+     * @param url - URL for the request, relative to the base URL used when initializing the API class
+     * @param method - HTTP method (e.g. GET/POS)
+     * @param data - Data to be sent to the server, in plain text
      * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
      */
-    _request(url, method, data) {
-        let retries = 5;
+    _request(url: URL | string, method: Method, data: string = "") : Promise<Object> {
+        const retries =  5;
         let sleep = 500;
         let params = {
             method: method,
+            body: data.length > 0 ? new Blob([data], {type: 'text/plain'}) : undefined
         };
-        if (typeof(data) !== 'undefined') {
-            params.body = new Blob([data], {type: 'text/plain'});
-        }
         return new Promise(async (resolve, reject) => {
             let response = null;
+            let error_save;
             for(let i = 0; i < retries; i++) {
                 try {
-                    response = await fetch(this.host + url, params);
+                    response = await fetch(new URL(url, this.host), params);
                     break;
                 }
                 catch (error) {
                     // sleep for a bit
+                    error_save = error;
                     await (new Promise(r => setTimeout(r, sleep)));
                     sleep *= 2;
                 }
@@ -42,7 +49,7 @@ class API {
 
             if (response == null) {
                 // out of retries
-                reject(error);
+                reject(error_save);
                 return;
             }
 
@@ -62,38 +69,38 @@ class API {
 
     /**
      * Performs an HTTP GET request
-     * @param {string} url - URL for the request, relative to the base URL used when initializing the API class
-     * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
+     * @param url - URL for the request, relative to the base URL used when initializing the API class
+     * @returns a promise that contains the parsed JSON returned from the server
      */
-    _get(url) {
-        return this._request(url, 'GET');
+    _get(url: URL|string) : Promise<Object> {
+        return this._request(url, 'GET', undefined);
     }
 
     /**
      * Performs an HTTP POST request
-     * @param {string} url - URL for the request, relative to the base URL used when initializing the API class
-     * @param {string} data - Data to be sent to the server, in plain text
+     * @param url - URL for the request, relative to the base URL used when initializing the API class
+     * @param data - Data to be sent to the server, in plain text
      * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
      */
-    _post(url, data) {
+    _post(url: URL|string, data: string = "") : Promise<Object> {
         return this._request(url, 'POST', data);
     }
 
     /**
      * Start a new participant session on the server
-     * @param {string} access_key - Access key for the experiment
+     * @param access_key - Access key for the experiment
      * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
      */
-    sessionStart(access_key) {
+    sessionStart(access_key: string) : Promise<Object> {
         return this._post(`${access_key}/participant/`);
     }
 
     /**
      * Start a new participant session on the server
-     * @param {string} access_key - Access key for the experiment
-     * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
+     * @param access_key - Access key for the experiment
+     * @returns a promise that contains the parsed JSON returned from the server
      */
-    sessionUpload(access_key, session_id, data) {
+    sessionUpload(access_key: string, session_id: string, data: string) : Promise<Object> {
         return this._post(`${access_key}/upload/${session_id}/`, data);
     }
 }
